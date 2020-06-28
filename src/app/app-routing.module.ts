bb6d53d4f07cd509142f2e77cbc87e3c3d0b302d
nazik naweed <nazik.naweed24@gmail.com>
2020-06-28 10:54:10 +0530

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './user/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'user/register',pathMatch:'full' },
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
